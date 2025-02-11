66897225432d3ec90d82eba4a4bf3ec8e6c39426
clarator <116170772+clarator@users.noreply.github.com>
2025-02-11 14:11:40 +0100

    mise a jour

---
+++ back/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+
+//servir les fichers du dossier "front"
+app.use(express.static(path.join(__dirname,"../front")));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname,'../front/index.html'));
+});
+
+const PORT = 4000;
+app.listen(PORT, () => {
+    console.log(`Serveur démarré sur http://localhost:${PORT}`);
+});
